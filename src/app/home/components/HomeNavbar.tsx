0121f5988bb20a27df5b88df21ee7370fa69ca3b
harsh12codes <harsh12codes@gmail.com>
2025-04-07 20:15:01 +0530

    feat: home-navbar, home-hero-section

---
+++ src/app/home/components/HomeNavbar.tsx
@@ -0,0 +1,41 @@
+import { Icons } from "@/assets/icons";
+import { ThemeToggle } from "@/components/theme/ThemeToggle";
+import { GithubIcon } from "lucide-react";
+import { Link, NavLink } from "react-router";
+
+export function HomeNavbar() {
+    return (
+        <div className="w-full max-w-6xl mx-auto flex items-center justify-between p-4">
+            <div className="flex items-center justify-center gap-1">
+                <Icons.LogoTiptor className="size-6" />
+                <span className="text-xl font-medium">Tiptor</span>
+            </div>
+
+            <div className="flex items-center justify-center gap-4">
+                <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                        isActive ? "text-foreground" : "text-muted-foreground"
+                    }
+                >
+                    Default
+                </NavLink>
+                <NavLink
+                    to="/notion-style"
+                    className={({ isActive }) =>
+                        isActive ? "text-foreground" : "text-muted-foreground"
+                    }
+                >
+                    Notion
+                </NavLink>
+            </div>
+            <div className="flex items-center justify-center gap-4 max-xs:hidden">
+                <Link to="https://github.com/harshwasthere/tiptor.git" target="_blank">
+                    <GithubIcon className="size-5 cursor-pointer" />
+                </Link>
+
+                <ThemeToggle />
+            </div>
+        </div>
+    );
+}
---
+++ src/app/home/index.tsx
@@ -0,0 +1,11 @@
+import { HomeNavbar } from "./components/HomeNavbar";
+import { HomeHeroSection } from "./components/HomeHeroSection";
+
+export default function HomePage() {
+    return (
+        <div>
+            <HomeNavbar />
+            <HomeHeroSection />
+        </div>
+    );
+}
