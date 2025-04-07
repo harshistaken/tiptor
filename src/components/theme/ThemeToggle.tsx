c6fc63751be928ed558647b3207a037bc987c131
harsh12codes <harsh12codes@gmail.com>
2025-04-07 20:14:36 +0530

    feat: added assets, dark mode, react-router, styles

---
+++ src/components/theme/ThemeToggle.tsx
@@ -0,0 +1,16 @@
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "./ThemeProvider";
+
+export function ThemeToggle() {
+    const { setTheme, theme } = useTheme();
+
+    return (
+        <div
+            className="relative size-5 cursor-pointer"
+            onClick={() => (theme === "dark" ? setTheme("light") : setTheme("dark"))}
+        >
+            <Sun className="absolute size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        </div>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import App from "./App.tsx";
+
+import { ThemeProvider } from "./components/theme/ThemeProvider.tsx";
+import { BrowserRouter } from "react-router";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <BrowserRouter>
+            <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+                <App />
+            </ThemeProvider>
+        </BrowserRouter>
+    </StrictMode>,
+);
