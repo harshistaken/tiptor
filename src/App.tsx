c0640dbc7d7bfdb3ce45111572d1e057feaff8a3
harsh12codes <harsh12codes@gmail.com>
2025-04-26 13:20:27 +0530

    feat: scrollbar styles & tiptap link extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import HomePage from "./web/home";
+import { DefaultEditorTemplate } from "./templates/default-editor-template";
 
 function App() {
-    return <HomePage />;
+    return (
+        <div className="w-full min-h-screen flex items-center justify-center p-8">
+            <DefaultEditorTemplate />
+        </div>
+    );
 }
 
 export default App;
