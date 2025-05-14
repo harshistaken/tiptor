2e9594223a19e3491af46c1bef7403a217bac9dc
harsh12codes <harsh12codes@gmail.com>
2025-05-14 07:30:39 +0530

    chore: minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { DefaultEditorTemplate } from "./templates/default-editor-template";
-
 function App() {
     return (
-        <div className="w-full min-h-screen flex items-center justify-center p-8">
-            <DefaultEditorTemplate />
-        </div>
+        <div className="w-full min-h-screen flex items-center justify-center p-8">Hello world</div>
     );
 }
 
